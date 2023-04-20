a5d9bf928074a96b0f1928741d6f80b5816c269f
ShrutiKalekar <shrutikalekar090@gmail.com>
2023-04-20 15:28:02 +0530

    update

--- src/NFCWriter.js
+++ src/NFCWriter.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function NFCWriter() {
   const [output, setOutput] = useState("");
-  const [status, setStatus] = useState(
-    "Web NFC is not available. Please make sure the 'Experimental Web Platform features' flag is enabled on Android."
-  );
+  const [url,setURL] = useState(null);
+  const [status, setStatus] = useState(()=> {
+    if(!('NDEFReader'in window)){
+        return "Web NFC is not available. Please make sure the 'Experimental Web Platform features' flag is enabled on Android."
+    } else {
+        return null;
+    }
+  });
 
   const handleScanClick = async () => {
     setOutput("User clicked scan button");
@@ -39,6 +45,68 @@ function NFCWriter() {
     }
   };
 
+  const writeGoogle = async () => {
+    const {data } = await axios.get(
+        `http://localhost:2160/link/google`,
+    );
+    console.log(data[0].link);
+    const ndef = new window.NDEFReader();
+    ndef.write({
+        records: [{ recordType: "url", data: data[0].link }]
+      }).then(() => {
+        console.log("Google link added.");
+        setOutput("Google link added.")
+      }).catch(error => {
+        console.log(`Write failed :-( try again: ${error}.`);
+        setOutput(`Write failed :-( try again: ${error}.`)
+      });
+  }
+
+  const writeLinkedIn = async () => {
+    const {data } = await axios.get(
+        `http://localhost:2160/link/linkedin`,
+    );
+    console.log(data[0].link);
+    const ndef = new window.NDEFReader();
+    ndef.write({
+        records: [{ recordType: "url", data: data[0].link }]
+      }).then(() => {
+        console.log("LinkedIn link added.");
+        setOutput("LinkedIn link added.")
+      }).catch(error => {
+        console.log(`Write failed :-( try again: ${error}.`);
+        setOutput(`Write failed :-( try again: ${error}.`)
+      });
+  }
+
+  const writeWApp = async() => {
+    const {data } = await axios.get(
+        `http://localhost:2160/link/whatsapp`,
+    );
+    console.log(data[0].link);
+    const ndef = new window.NDEFReader();
+    ndef.write({
+        records: [{ recordType: "url", data: data[0].link }]
+      }).then(() => {
+        console.log("WhatsApp link added.");
+        setOutput("WhatsApp link added.")
+      }).catch(error => {
+        console.log(`Write failed :-( try again: ${error}.`);
+        setOutput(`Write failed :-( try again: ${error}.`)
+      });
+  }
+
+  const writeURL = async() => {
+    console.log(url);
+    try {
+      const ndef = new window.NDEFReader();
+      await ndef.write(url);
+      setOutput(`URL Written ${url}`);
+    } catch (error) {
+        setOutput(`Write failed! ${error}`);
+    }
+  }
+
   return (
     <div>
       <h3>Live Output</h3>
@@ -47,12 +115,19 @@ function NFCWriter() {
         <div id="status">{status}</div>
         <pre id="log">{output}</pre>
       </div>
-      <button id="scanButton" onClick={handleScanClick}>
+      {/* <button id="scanButton" onClick={handleScanClick}>
         Scan
-      </button>
-      <button id="writeButton" onClick={handleWriteClick}>
+      </button> */}
+      {/* <button id="writeButton" onClick={handleWriteClick}>
         Write
-      </button>
+      </button> */}
+      <br/>
+      <button class='btn' onClick={writeGoogle}>Google</button>
+      <button onClick={writeLinkedIn}>LinkedIn</button>
+      <button onClick={writeWApp}>What'sApp</button>
+      <h2>Write any URL</h2>
+      <input type="url" onChange={(e)=> setURL(e.target.value)}></input>
+      <button onClick={writeURL}>Write</button>
     </div>
   );
 }
