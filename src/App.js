c1f52c12f7d12c9f341b6ba725242ef02d4f3888
mustali17 <mustalichunawala@gmail.com>
2023-09-02 11:05:35 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import axios from 'axios';
+import axios from "axios";
 import NfcWriter from "./NFCWriter";
 // import ndef from 'ndef';
-
 
 function App() {
   return (
     <div className="App">
-        <h1>Write URL to NFC tag</h1>
+      <h1>Write URL to NFC tag test-1</h1>
       <main>
         <NfcWriter />
       </main>
